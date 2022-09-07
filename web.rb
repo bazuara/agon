203816c043a9a25128ef41df28a88eb3f057b8ea
Bruno Azuara <bruno.azuara@gmail.com>
2022-09-07 17:35:41 +0200

    user view

--- web.rb
+++ web.rb
@@ -8,9 +8,15 @@ require 'omniauth-marvin'
 require_relative 'tools/api42'
 
 # Static Background URL
-metropolis_background = 'https://cdn.intra.42.fr/coalition/cover/64/Portada_Metropolis_2.jpg'
-atlantis_background = 'https://cdn.intra.42.fr/coalition/cover/65/Portada_Atlantis_2.jpg'
-wakanda_background = 'https://cdn.intra.42.fr/coalition/cover/66/Portada_Wakanda_2_min.jpg'
+
+def coa_bg(id)
+  metropolis_bg = 'https://cdn.intra.42.fr/coalition/cover/64/Portada_Metropolis_2.jpg'
+  atlantis_bg = 'https://cdn.intra.42.fr/coalition/cover/65/Portada_Atlantis_2.jpg'
+  wakanda_bg = 'https://cdn.intra.42.fr/coalition/cover/66/Portada_Wakanda_2_min.jpg'
+  return metropolis_bg if id == 64
+  return atlantis_bg if id == 65
+  return wakanda_bg if id == 66
+end
 
 # enable sessions
 configure do
@@ -24,7 +30,8 @@ end
 
 post '/search' do
   @coa_user = Coalition_user.new
-  @coa_user.search(params[:login])
+  @login = params[:login]
+  @coa_user.search(@login)
   if @coa_user.name.nil?
     session[:user_not_found] = true
     redirect '/'
